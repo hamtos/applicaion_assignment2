a38f7092a7d1744254eca015b6285a63cdea0f82
hamtos <135288715+hamtos@users.noreply.github.com>
2023-06-27 14:26:48 +0000

    [add]comment_create

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,7 +1,26 @@
 class BookCommentsController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book)
+    else
+      render template: 'books/show'
+    end
+
   end
 
   def destroy
+    book = Book.find(params[:book_id])
+    book_comment = book.book_comment.find(params[:id])
+    book_comment.destroy
+    redirect_to book_path(book)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:body, :user_id, :book_id)
   end
 end
