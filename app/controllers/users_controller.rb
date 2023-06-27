7a03ee4fdf57da0a601173774755751ad2df1289
hamtos <135288715+hamtos@users.noreply.github.com>
2023-06-27 09:47:27 +0000

    [add]favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:update, :edit, :destroy]
 
   def show
     @user = User.find(params[:id])
@@ -13,10 +13,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to user_path(current_user)
-    end
   end
 
   def update
